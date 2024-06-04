2b8229434004822f6832c614596cb14490c113ae
Marcus <Marcusadonis5@gmail.com>
2024-06-05 00:21:05 +0200

    Updatev3

--- script/index.js
+++ script/index.js
@@ -1,7 +1,8 @@
+//made my varibles im going to use in the calculations
 let age = document.querySelector('input')
 let converter = document.querySelector('button')
 let answer = document.querySelector('#Answer')
 
 converter.addEventListener('click',()=>{
-    answer.innerText = ('your age in days: ' + (eval(`${age.value} *365`)))
+    answer.innerText = ('your age in days: ' + (eval(`${age.value} *365`)))  // using the single input and multiplying it by 365 
 })
